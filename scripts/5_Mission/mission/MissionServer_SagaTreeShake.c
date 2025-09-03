9e3c575ab8b09445db2e3dfc2f0a8b25c0e46167
hummigithub <75567065+hummigithub@users.noreply.github.com>
2025-09-03 11:43:00 +0200

    Delete scripts directory
    New Version

--- scripts/5_Mission/mission/MissionServer_SagaTreeShake.c
+++
@@ -1,9 +0,0 @@
-modded class MissionServer
-{
-    override void OnInit()
-    {
-        super.OnInit();
-        // Erzwingt Erstellen/Laden der Profiles-Config beim Serverstart
-        SagaTreeShakeSettings.Get();
-    }
-}
